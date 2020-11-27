4cad7d413af1072b7f468bd436ccd597b1519920
Ezarsatria12 <Ezar3004>
2020-11-27 15:47:32 +0700

    school assignment

---
+++ app/src/main/java/com/ezar/myflexiblefragment/ProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.ezar.myflexiblefragment
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nunu.myflexiblefragment.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
